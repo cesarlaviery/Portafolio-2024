446d2ab6ba5857a3bde8de92d4f32828baf45aa1
Cesar Laviery <141261537+cesarlaviery@users.noreply.github.com>
2024-05-27 11:44:18 -0400

    update data

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const navBarLogo = document.querySelector(".navbar__logo");
 navBarLogo.addEventListener("click", () => {
     navBar.classList.toggle("navbar--active");
     
+    navBarLogo.classList.toggle("navbar__logo--active")
+    
     navBarItems.forEach(element => {
         element.classList.toggle("navbar__item--active");
     });
