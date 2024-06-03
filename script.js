51012042fef8b99447a04bd4040613b1e352b6b9
Cesar Laviery <141261537+cesarlaviery@users.noreply.github.com>
2024-06-03 11:16:14 -0400

    update data

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+// Particles.js
+
+particlesJS.load('particles-js', 'assets/JS/particlesjs-config.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+
+// NavBar
+
 const navBar = document.querySelector(".navbar");
 const navBarItems = document.querySelectorAll(".navbar__item")
 const navBarLogo = document.querySelector(".navbar__logo");
 
 navBarLogo.addEventListener("click", () => {
     navBar.classList.toggle("navbar--active");
-    
+
     navBarLogo.classList.toggle("navbar__logo--active")
-    
+
     navBarItems.forEach(element => {
         element.classList.toggle("navbar__item--active");
     });
+});
+
+// Typed.js
+
+const typed = new Typed(".typed", {
+    strings: ["Bienvenido.", "Welcome."],
+    stringsElement: '#cadenas-texto', 
+    typeSpeed: 75,
+    startDelay: 300, 
+    backSpeed: 75, 
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500, 
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
 });
