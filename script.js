6ad83bf027818976ff3d8646d0996bc3f6c4bb17
Cesar Laviery <141261537+cesarlaviery@users.noreply.github.com>
2024-06-04 12:34:52 -0400

    update data

--- script.js
+++ script.js
@@ -24,16 +24,27 @@ navBarLogo.addEventListener("click", () => {
 
 const typed = new Typed(".typed", {
     strings: ["Bienvenido.", "Welcome."],
-    stringsElement: '#cadenas-texto', 
+    stringsElement: '#cadenas-texto',
     typeSpeed: 75,
-    startDelay: 300, 
-    backSpeed: 75, 
+    startDelay: 300,
+    backSpeed: 75,
     smartBackspace: true,
     shuffle: false,
-    backDelay: 1500, 
+    backDelay: 1500,
     loop: true,
     loopCount: false,
     showCursor: true,
     cursorChar: '|',
     contentType: 'html',
+});
+
+// Start
+
+const translateIcon = document.querySelector(".start-icon");
+const start = document.querySelector(".start");
+
+translateIcon.addEventListener("click", () => {
+
+    start.classList.toggle("start__paragraph-opacity");
+
 });
